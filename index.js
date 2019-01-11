c2a21f81da9beb23bf9e1c8a7f01ae0d72dee8b8
unknown <prraful@gmail.com>
2019-01-11 18:16:07 +0530

    express with ejs

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 /**
  * Express code will come here!!!!
  */
+//do all imports
+ var express = require('express')
+ var ejs = require('ejs')
+ var bp = require('body-parser')
+  
+ //initialize the express app
+ var app = express()
+  
+ //set the view engine
+ app.set('view engine', 'ejs')
+ //set the folder where all views will be present
+ app.set('views',__dirname + '/template')
+ 
+ app.use(bp.json())   
+
+ var urlencodeparser = bp.urlencoded({extended:false})
+
+ app.get("/", function(req, res){
+     res.send("Hello from Express!!!!")
+ })
+
+ app.get("/home", function(req, res){
+    res.render('home')
+})
+
+app.get("/about", function(req, res){
+    res.render('about')
+})
+
+app.post("/about", urlencodeparser, function(req, res){
+    console.log(req.body)
+    res.send("Received information: " + JSON.stringify(req.body) )
+})
+
+app.get('/contact/:name', function(req, res){
+    console.log("Get in touch with: " + req.params.name)
+    console.log(req.body)
+    
+    //res.send("received Data")
+    res.render('profile', {
+                            name:req.params.name,
+                            profile:req.body
+                         })
+})
+
+
+ app.listen(8888)
